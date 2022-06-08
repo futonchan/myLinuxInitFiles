95fea0afa5a521c0a4c99822dfec96bcc82cd0b5
futonchan <assdroi555@gmail.com>
2022-06-08 16:35:52 +0900

    feat: githubの初期設定ファイルを追加

---
+++ lab/004_githubSetup.sh
@@ -0,0 +1,4 @@
+git config --global user.email "assdroi555@gmail.com"
+git config --global user.name "futonchan"
+# ssh-keygen -t ed25519
+# https://qiita.com/shizuma/items/2b2f873a0034839e47ce 手入力多いのでこっち参考に
